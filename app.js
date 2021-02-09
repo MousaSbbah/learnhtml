3d2a56899b1e3c7e806a48354465fa1965c1c988
Mousa Sabah <mousa@DESKTOP-KQRJ9RV.localdomain>
2021-02-09 16:49:01 +0200

    addFunction

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ var yname =prompt('please enter your name')
         var old =2021 -  prompt('What is you birth Year')
         if (old <= 130 ){
             document.write('And your age is a ' + old + 'years ')
-        } else { document.write('This is not a real birth year , please refresh and try again')}        
+        } else { document.write('This is not a real birth year , please refresh and try again')}   
+        
+        
